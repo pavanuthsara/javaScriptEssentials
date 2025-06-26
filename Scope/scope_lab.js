ee3deef8890866a43853d58788c6fc0b50914057
Pavan Uthsara <uthsarapavan@gmail.com>
2025-06-26 00:24:11 -0400

    scope v2

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -16,5 +16,16 @@ console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
 
 //block scope
-console.log(blockVar);
-console.log(blockLet);
+// console.log(blockVar);
+// console.log(blockLet);
+
+function show(){
+var functionVar = "I'm a block-scoped var";
+let functionLet = "I'm a block-scoped let";
+const functionConst = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
